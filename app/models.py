6883aa19c3247176b4a7847bf442a709cf0e300f
Nasir Khan <nk99technology@gmail.com>
2024-10-15 12:39:09 +0600

    create a new app, setup author and book model, add requirements.txt

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    published_date = models.DateField()
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
